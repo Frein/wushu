2dd9fdd0829c77ba135402c8529d2a179bd750f1
Sergei <gross17@yandex.ru>
2020-04-20 18:05:25 +0300

    fix

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ const path = require('path');
 const app = express();
 app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('/ping', function (req, res) {
+app.use('/ping', function (req, res) {
     return res.send({point:123});
 });
 
-app.get('/', function (req, res) {
+app.get('*', function (req, res) {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
